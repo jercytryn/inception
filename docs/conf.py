7daaf5274c16dbfe5018f6e513b0dabd76b9540d
Jeremy Cytryn <jercytryn@gmail.com>
2015-05-20 05:51:20 -0400

    New html docs generation setup
    See http://daler.github.io/sphinxdoc-test/includeme.html

--- docs/conf.py
+++ docs/conf.py
@@ -14,6 +14,7 @@
 import sys
 import os
 import shlex
+import alabaster
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
@@ -30,6 +31,7 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.pardir, 'src')))
 # ones.
 extensions = [
     'sphinx.ext.autodoc',
+    'alabaster'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -117,7 +119,7 @@ html_theme = 'alabaster'
 #html_theme_options = {}
 
 # Add any paths that contain custom themes here, relative to this directory.
-#html_theme_path = []
+html_theme_path = [alabaster.get_path()]
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
